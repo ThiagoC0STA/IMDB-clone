0619dac250745c8d40a6f41b7de5a97aaf4d52cb
Thiago Rodrigues da Costa <82855801+ThiagoC0STA@users.noreply.github.com>
2023-04-13 10:15:34 -0300

    creation of about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600 ">About</h1>
+      <p>
+        Welcome to IMDb - the world&apos;s largest database of movies, TV shows,
+        and cast information. Since 1990, we have been the most trusted source
+        of information about the world of entertainment.
+      </p>
+      <p>
+        With over 6 million titles in our database, you can find detailed
+        information about any movie or TV show you can imagine. We provide our
+        users with synopses, trailers, movie reviews, ratings, full cast
+        information, and much more. Additionally, you can see the latest news
+        from the world of entertainment and discover upcoming releases from
+        around the world.
+      </p>
+
+      <p>
+        IMDb is the perfect place for movie and TV lovers to find everything
+        they need to know about their favorite movies and shows. So, explore our
+        site and find your next favorite movie today!
+      </p>
+    </div>
+  );
+}
