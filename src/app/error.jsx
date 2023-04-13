f83e1ec35fd46b498e5eb78dcdde3310643be45b
Thiago Rodrigues da Costa <82855801+ThiagoC0STA@users.noreply.github.com>
2023-04-13 19:35:01 -0300

    handle errors

---
+++ src/app/error.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  return (
+    <div className="text-center mt-14 ">
+      <h1>Something went wrong</h1>
+      <button className="hover:text-amber-600 transition" onClick={() => reset()}>Try again</button>
+    </div>
+  );
+}
